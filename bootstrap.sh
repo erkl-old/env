1ca53706187177b4e71b229d2c475023894b3f75
Erik Lundin <erklundin@gmail.com>
2014-10-25 21:17:25 +0100

    Add bootstrap.sh script

---
+++ bootstrap.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+# Stop at the first sign of trouble.
+set -e
+set -x
+
+# Download and decrypt the public and private keys.
+mkdir -p ~/.ssh
+wget -qO - https://github.com/erkl/env/raw/master/ssh/keys/id_rsa.encrypted | openssl enc -aes-256-cbc -d > ~/.ssh/id_rsa
+wget -qO - https://github.com/erkl/env/raw/master/ssh/keys/id_rsa.pub.encrypted | openssl enc -aes-256-cbc -d > ~/.ssh/id_rsa.pub
+
+# Set file permissions.
+chmod 0700 ~/.ssh
+chmod 0600 ~/.ssh/id_rsa
+chmod 0644 ~/.ssh/id_rsa.pub
+
+# Clone the whole repository.
+pacman -S git
+git clone git@github.com:erkl/env.git ~/.env
