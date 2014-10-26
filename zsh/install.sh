09f47dda4b141300a0a8888c54821d7199e05c60
Erik Lundin <erklundin@gmail.com>
2014-10-26 19:42:19 +0000

    zsh: Install .zshrc with the install.sh script

--- zsh/install.sh
+++ zsh/install.sh
@@ -6,3 +6,6 @@ set -ex
 # Install zsh and make it the current user's default shell.
 sudo pacman -S --noconfirm zsh
 chsh -s /bin/zsh
+
+# Install .zshrc.
+ln -s "$HOME/.env/zsh/.zshrc" "$HOME/.zshrc"
