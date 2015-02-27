5cf73a3469fd73ef17940d3d85fb8495cff219d3
Erik Lundin <erklundin@gmail.com>
2015-02-27 23:54:07 +0000

    ssh: Move key decryption out from the root install.sh script

--- install.sh
+++ install.sh
@@ -4,18 +4,7 @@ set -ex
 # Install dependencies.
 sudo pacman -S --noconfirm wget git openssh
 
-# Download and decrypt the public and private keys.
-if [ ! -e "$HOME/.ssh/id_rsa" -o ! -e "$HOME/.ssh/id_rsa.pub" ]; then
-    mkdir -p "$HOME/.ssh"
-    wget -qO - https://github.com/erkl/env/raw/master/ssh/keys/id_rsa.encrypted | openssl enc -aes-256-cbc -d > "$HOME/.ssh/id_rsa"
-    wget -qO - https://github.com/erkl/env/raw/master/ssh/keys/id_rsa.pub.encrypted | openssl enc -aes-256-cbc -d > "$HOME/.ssh/id_rsa.pub"
-
-    chmod 0700 "$HOME/.ssh"
-    chmod 0600 "$HOME/.ssh/id_rsa"
-    chmod 0644 "$HOME/.ssh/id_rsa.pub"
-fi
-
-# Clone the whole repository.
+# Clone the repository if this is our first time.
 if [ ! -e "$HOME/.env" ]; then
     git clone git@github.com:erkl/env.git "$HOME/.env"
 fi
