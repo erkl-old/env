f262b39705437c9a5830eea75fc63f62c56c456b
Erik Lundin <erklundin@gmail.com>
2015-02-28 00:08:00 +0000

    go: Remove double square brackets because we can

--- go/install.sh
+++ go/install.sh
@@ -4,7 +4,7 @@ set -ex
 # Pull down the prefered version if it's not already installed.
 VERSION="go1.3.3"
 
-if [[ ! -e "/usr/local/go/VERSION" || $(cat "/usr/local/go/VERSION") != "$VERSION" ]]; then
+if [ ! -e "/usr/local/go/VERSION" -o $(cat "/usr/local/go/VERSION") != "$VERSION" ]; then
 	rm -rf /usr/local/go
 	wget -qO - "https://storage.googleapis.com/golang/$VERSION.linux-amd64.tar.gz" | tar -C /usr/local -xzf
 
